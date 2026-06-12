6ab3003a856ee1e1dab79f521d851c03a590425b
aryanrajput9 <babusahebaryan4@gmail.com>
2026-06-12 15:29:04 +0530

    Create points table component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,8 @@
 import { createRoot } from "react-dom/client"
 import "./index.css";
 import AnalyticsPage from './feature/analytics/pages/AnalyticsPage.jsx'
+import PointsTable from "./feature/analytics/pages/PointsTable.jsx";
 
 createRoot(document.getElementById('root')).render(
-    <AnalyticsPage />
+    <PointsTable />
 )
