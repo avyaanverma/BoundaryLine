29df19f950d4238876f8ba78735b762f8848b5ac
avyaanverma <avyaanver@gmail.com>
2026-06-13 15:11:39 +0530

    pino logger added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 import createApp from "./src/app.js";
 import connectDB from "./src/config/db.js";
 import env from "./src/config/env.js";
+import { logger } from "./src/util/Logger.js";
 
 function startServer(){
     const app = createApp();
     connectDB().then(()=>{
-        console.log("database connected.")
+        logger.info("database connected.")
         app.listen(env.PORT, ()=>{
-            console.log(`Server listening on http://localhost:${env.PORT}`);
+            logger.info(`Server listening on http://localhost:${env.PORT}`);
         });
     }).catch((err)=>{
-        console.log("Error in connecting database : ", err)
+        logger.error("Error in connecting database : ", err)
     })
 }
 
