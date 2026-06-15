976dd8ea0dda52b01646ac09dd26efe250798b9d
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-15 18:46:41 +0530

    score interface

---
+++ server/src/modules/private/score/interface/score.interface.js
@@ -0,0 +1,18 @@
+/**
+ * Score metadata definition
+ */
+export const ScoreFields = {
+  id: "String",
+  matchId: "ObjectId",
+  innings: "Number",
+  battingTeam: "ObjectId",
+  score: "Number",
+  wickets: "Number",
+  overs: "String",
+  runRate: "Number",
+  target: "Number",
+  createdBy: "ObjectId",
+  updatedBy: "ObjectId",
+  createdAt: "Date",
+  updatedAt: "Date"
+};
