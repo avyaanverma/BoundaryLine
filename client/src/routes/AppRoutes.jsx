c6e10e4ee7197bce31d6969d329b9de6b423000e
aryanrajput9 <babusahebaryan4@gmail.com>
2026-06-16 14:26:38 +0530

    update

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,21 +6,12 @@ import ScorerConsolePage from "../features/scorer-console/pages/ScorerConsolePag
 import ScorerWorkspace from "../features/scorer-console/pages/ScorerWorkspace.jsx";
 import { RoleGuard } from "../app/guards/RoleGuard.jsx";
 import { UserRole } from "../features/scorer-console/pages/type.js";
-import AnalyticsPage from "../feature/analytics/pages/AnalyticsPage.jsx";
-import MainLayout from '../layout/MainLayout.jsx'
-import DuplicateFixture from "../features/fixtures/pages/duplicateFixture.jsx";
-import UserRegisterForm from "../features/auth/user/component/UserRegisterForm.jsx";
-import UserLoginForm from "../features/auth/user/component/UserLoginForm.jsx";
 import AdminRegisterForm from "../features/auth/admin/components/AdminRegisterForm.jsx";
 import { AdminLoginPage } from "../pages/admin/AdminLoginPage.jsx";
 import AdminDashboardPage from "../pages/admin/AdminDashboardPage.jsx";
 import NewsPage from "../features/news/pages/NewsPage.jsx";
 import TeamPage from "../feature/all-team/page/TeamPage.jsx";
 import RankingPage from "../features/ranking/pages/RankingPage.jsx";
-import CreateTournament from "../pages/Tournament/pages/CreateTournament.jsx";
-import CreateMatch from "../pages/Tournament/pages/CreateMatch.jsx";
-import CreatePlayer from "../pages/Tournament/pages/CreatePlayer.jsx";
-import CreateSeries from "../pages/Tournament/series/pages/CreateSeries.jsx";
 
 const ComingSoonPage = ({ title, description }) => {
   return (
